08d2c4253c4a5fe76786639541ca683337260ac7
Ferro Fang <fzinfz@gmail.com>
2017-08-10 01:28:14 +0800

    update

--- contacts/models.py
+++ contacts/models.py
@@ -4,3 +4,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     vcard = models.TextField()
     sync = models.NullBooleanField()
+
+    def __str__(self):
+        return 'Contact: ' + self.name
